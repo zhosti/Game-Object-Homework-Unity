d765f997e6c5d91323443d232f4986ce72f15ed8
Ivan <zhostovivan@gmail.com>
2014-11-10 17:47:36 +0200

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -2,20 +2,18 @@ using UnityEngine;
 using System.Collections;
 
 public class Player : MonoBehaviour {
-	
+
+
+
 	float speed = 3;
-
-	 Camera MainCam;
-	//GameObject Player;
-
-	void Star() {
-		MainCam = (Camera)GameObject.Find ("Main Camera").GetComponent<Camera>();
-		//Player = GameObject.Find("Player");
+	Camera MainCam;
+	// Use this for initialization
+	void Start () {
+		MainCam = (Camera)GameObject.Find ("Main Camera").GetComponent<Camera> ();
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-
 		if (Input.GetKey(KeyCode.UpArrow)) {
 			transform.Translate(0f,0f,speed * Time.deltaTime);
 		}
@@ -28,12 +26,9 @@ public class Player : MonoBehaviour {
 		else if (Input.GetKey(KeyCode.LeftArrow)) {
 			transform.Translate(-speed * Time.deltaTime,0f,0f);
 		}
-
-
 		float horizontal = 3.0f * Input.GetAxis("Mouse X");
 		float vertical = 3.0f * Input.GetAxis("Mouse Y");
 		transform.Rotate(0f, horizontal, 0f);
-		MainCam.transform.Rotate(vertical, 0f, 0f);
-
+		MainCam.transform.Rotate(-vertical, 0f, 0f);
 	}
 }
